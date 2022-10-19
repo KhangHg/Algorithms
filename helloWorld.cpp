b64f0f038ccc55359f9f7a99885b03dd6b7e845b
Cashsu <nghia.hnkt@gmail.com>
2022-10-19 22:30:39 +0700

    init code

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -2,11 +2,8 @@
 using namespace std;
 int main()
 {
-    for (int i = 0; i < 10; i++)
-    {
-        cout << "Hello world";
-        /* code */
-    }
+
+    cout << "Hello world\n";
 
     return 0;
 }
