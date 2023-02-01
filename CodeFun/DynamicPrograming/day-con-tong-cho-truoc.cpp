5e2b4e542051c67234ec289bdd80d69590e0eaea
Hoàng Như Nghĩa BAT Tech <nghiahn@batgroup.vn>
2023-02-01 16:53:41 +0700

    update

--- CodeFun/DynamicPrograming/day-con-tong-cho-truoc.cpp
+++ CodeFun/DynamicPrograming/day-con-tong-cho-truoc.cpp
@@ -5,12 +5,35 @@ using namespace std;
 const int MAX = 100;
 
 int n, a[MAX], S;
-
+int c[MAX][1000];
+// top down
 int F(int s, int k)
 {
     if (k == 0)
         return (s == 0) ? 1 : 0;
     return (s < a[k]) ? F(s, k - 1) : F(s, k - 1) + F(s - a[k], k - 1);
+}
+
+// bottom up
+void B(int s, int n)
+{
+    c[0][0] = 1;
+    for (int j = 1; j <= s; j++)
+    {
+        c[0][j] = 0;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= s; j++)
+        {
+            c[i][j] = c[i - 1][j];
+            if (j >= a[i])
+            {
+                c[i][j] += c[i - 1][j - a[i]];
+            }
+        }
+    }
 }
 
 int main()
@@ -24,5 +47,7 @@ int main()
 
     cout << "S = ";
     cin >> S;
-    cout << "So day con : " << F(S, n);
+    cout << "So day con (top down) : " << F(S, n) << endl;
+    B(S, n);
+    cout << "So day con ( bottom up) : " << c[n][S];
 }
